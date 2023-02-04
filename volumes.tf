883d6d52f5845fa19d60a3b2f5fb6b085e934237
flx5 <1330854+flx5@users.noreply.github.com>
2023-02-04 21:45:15 +0100

    debug github actions download

--- volumes.tf
+++ volumes.tf
@@ -15,4 +15,8 @@ resource "null_resource" "debug" {
 resource "libvirt_volume" "coreos" {
   name   = "os_image-coreos"
   source = module.coreos-image.image_path
+
+  depends_on = [
+    module.coreos-image
+  ]
 }
