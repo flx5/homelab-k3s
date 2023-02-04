64e6399273b861bb80f59e4f6e12d2a07ae8e27e
flx5 <1330854+flx5@users.noreply.github.com>
2023-02-04 22:02:24 +0100

    disable kvm on actions

--- volumes.tf
+++ volumes.tf
@@ -2,16 +2,6 @@ module "coreos-image" {
   source = "./download_coreos"
 }
 
-resource "null_resource" "debug" {
-  provisioner "local-exec" {
-    command = "pwd && tree .terraform"
-  }
-
-  depends_on = [
-    module.coreos-image
-  ]
-}
-
 resource "libvirt_volume" "coreos" {
   name   = "os_image-coreos"
   source = module.coreos-image.image_path
