c6396ca160d54cf4d8281927f36406377729efcf
flx5 <1330854+flx5@users.noreply.github.com>
2023-02-04 21:32:28 +0100

    debug github actions download

--- volumes.tf
+++ volumes.tf
@@ -2,6 +2,12 @@ module "coreos-image" {
   source = "./download_coreos"
 }
 
+resource "null_resource" "debug" {
+  provisioner "local-exec" {
+    command = "ls '${module.coreos-image.image_path}'"
+  }
+}
+
 resource "libvirt_volume" "coreos" {
   name   = "os_image-coreos"
   source = module.coreos-image.image_path
